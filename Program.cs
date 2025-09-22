da2db687ce70e9ab9b92e103d5040cad372c7d22
Brandon-Knorr <bknorr@my.wctc.edu>
2025-09-21 21:33:00 -0500

    j

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
-// Connection info stored in appsettings.json
-IConfiguration configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultSQLiteConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Medals API",
+        Version = "v1",
+        Description = "Olympic Medals API",
+    });
+    c.EnableAnnotations();
+});
+
+// Add CORS services to the container.
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "Open",
@@ -18,33 +32,19 @@ builder.Services.AddCors(options =>
                 .AllowAnyHeader();
         });
 });
-// Register the DataContext service
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(configuration["ConnectionStrings:DefaultSQLiteConnection"]));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { 
-        Title = "Medals API", 
-        Version = "v1",
-        Description = "Olympic Medals API",
-    });
-    c.TagActionsBy(api => [api.HttpMethod]);
-    c.EnableAnnotations();
-});
 
 var app = builder.Build();
 
-app.UseCors("Open");
-
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-// }
+}
+
+// THIS IS THE KEY CHANGE -> UseCors must come before UseAuthorization
+app.UseCors("Open");
 
 app.UseHttpsRedirection();
 
